ErikReider/SwaySettings

commit 8c3a59740be93d8317060960af5ccfa2c1f48219

Destroy pages when going back to the main page

src/Pages/Users/Users.vala
@@ -15,8 +15,8 @@ namespace SwaySettings {
 
         private Users_Content content;
 
-        public Users (string label, Hdy.Deck deck, IPC ipc) {
-            base (label, deck, ipc);
+        public Users (string label, Hdy.Deck deck) {
+            base (label, deck);
         }
 
         public override Gtk.Widget set_child () {
@@ -63,7 +63,7 @@ namespace SwaySettings {
 
             if (current_user == null || !current_user.is_loaded) {
                 current_user = user_manager.get_user (username);
-                current_user.notify["is-loaded"].connect (set_user_data);
+                current_user.notify["is-loaded"].connect ((er,t) => {});
                 current_user.changed.connect (set_user_data);
             } else {
                 set_user_data ();

src/Page/Input_Page.vala
@@ -28,8 +28,11 @@ namespace SwaySettings {
 
         public abstract Input_Types input_type { get; }
 
+        IPC ipc;
+
         protected Input_Page (string label, Hdy.Deck deck, IPC ipc) {
-            base (label, deck, ipc);
+            base (label, deck);
+            this.ipc = ipc;
         }
 
         public virtual ArrayList<Input_Page_Section> get_top_sections () {

src/Page/Page.vala
@@ -16,13 +16,10 @@ namespace SwaySettings {
 
         public string label;
 
-        public IPC ipc;
-
         public virtual bool refresh_on_realize { get; default = true; }
 
-        protected Page (string label, Hdy.Deck deck, IPC ipc) {
+        protected Page (string label, Hdy.Deck deck) {
             Object ();
-            this.ipc = ipc;
             this.label = label;
             header_bar.set_title (this.label);
             back_button.clicked.connect (() => {
@@ -37,7 +34,7 @@ namespace SwaySettings {
         public virtual void on_refresh () {
         }
 
-        public virtual void on_back (Hdy.Deck deck) {
+        public virtual async void on_back (Hdy.Deck deck) {
         }
 
         public static Hdy.Clamp get_clamped_widget (Gtk.Widget widget,
@@ -106,8 +103,8 @@ namespace SwaySettings {
             }
         }
 
-        protected Page_Scroll (string label, Hdy.Deck deck, IPC ipc) {
-            base (label, deck, ipc);
+        protected Page_Scroll (string label, Hdy.Deck deck) {
+            base (label, deck);
         }
 
         public override void on_refresh () {
@@ -134,9 +131,8 @@ namespace SwaySettings {
 
         protected Page_Tabbed (string label,
                                Hdy.Deck deck,
-                               IPC ipc,
                                string no_tabs_text = "Nothing here...") {
-            base (label, deck, ipc);
+            base (label, deck);
 
             stack = new Gtk.Stack ();
             stack.transition_type = Gtk.StackTransitionType.CROSSFADE;
@@ -176,7 +172,7 @@ namespace SwaySettings {
             }
         }
 
-        public override void on_back (Hdy.Deck deck) {
+        public override async void on_back (Hdy.Deck deck) {
             stack.set_visible_child (stack.get_children ().nth_data (0));
         }
 

src/Pages/Background/Background_Page.vala
@@ -17,8 +17,11 @@ namespace SwaySettings {
         private int list_image_height = 144;
         private int list_image_width = 256;
 
+        IPC ipc;
+
         public Background_Page (string page_name, Hdy.Deck deck, IPC ipc) {
-            base (page_name, deck, ipc);
+            base (page_name, deck);
+            this.ipc = ipc;
         }
 
         public override void on_refresh () {

src/Pages/Bluetooth/Bluetooth_Page.vala
@@ -24,8 +24,8 @@ namespace SwaySettings {
 
         Bluez.Daemon daemon;
 
-        public Bluetooth_Page (string page_name, Hdy.Deck deck, IPC ipc) {
-            base (page_name, deck, ipc);
+        public Bluetooth_Page (string page_name, Hdy.Deck deck) {
+            base (page_name, deck);
         }
 
         public override void on_back (Hdy.Deck deck) {

src/Pages/DefaultApps/Default_Apps.vala
@@ -15,8 +15,8 @@ namespace SwaySettings {
             default_app_data ("File Browser", "inode/directory"),
         };
 
-        public Default_Apps (string page_label, Hdy.Deck deck, IPC ipc) {
-            base (page_label, deck, ipc);
+        public Default_Apps (string page_label, Hdy.Deck deck) {
+            base (page_label, deck);
         }
 
         public override Gtk.Widget set_child () {

src/Pages/StartupApps/Startup_Apps.vala
@@ -7,8 +7,8 @@ namespace SwaySettings {
 
         ArrayList<DesktopAppInfo> startup_apps;
 
-        public Startup_Apps (string page_label, Hdy.Deck deck, IPC ipc) {
-            base (page_label, deck, ipc);
+        public Startup_Apps (string page_label, Hdy.Deck deck) {
+            base (page_label, deck);
         }
 
         public override Gtk.Widget set_child () {

src/Pages/Swaync/Swaync.vala
@@ -54,9 +54,11 @@ namespace SwaySettings {
     public class Swaync : Page_Scroll {
 
         ConfigModel settings;
+        IPC ipc;
 
         public Swaync (string label, Hdy.Deck deck, IPC ipc) {
-            base (label, deck, ipc);
+            base (label, deck);
+            this.ipc = ipc;
         }
 
         public override Gtk.Widget set_child () {

commit 38f2d7a9488d4cef66a81589e9ca341aa39b411f

Fixed Users page regression caused by 8c3a5974

src/Pages/Users/Users.vala
@@ -10,17 +10,32 @@ namespace SwaySettings {
     private errordomain PathError { INVALID_PATH }
 
     public class Users : Page_Scroll {
-        private unowned Act.UserManager user_manager = Act.UserManager.get_default ();
-        private unowned Act.User current_user;
+        private static unowned Act.UserManager user_manager = Act.UserManager.get_default ();
+        private static unowned Act.User current_user = user_manager.get_user (
+            Environment.get_user_name ());
 
         private Users_Content content;
+
+        private ulong is_loaded_id = 0;
+        private ulong changed_id = 0;
 
         public Users (string label, Hdy.Deck deck) {
             base (label, deck);
         }
 
+        public override async void on_back (Hdy.Deck deck) {
+            // user_manager.
+            if (is_loaded_id != 0) {
+                current_user.disconnect (is_loaded_id);
+                is_loaded_id = 0;
+            }
+            if (changed_id != 0) {
+                current_user.disconnect (changed_id);
+                changed_id = 0;
+            }
+        }
+
         public override Gtk.Widget set_child () {
-            string username = GLib.Environment.get_user_name ();
             content = new Users_Content ();
 
             // Avatar EventBox
@@ -61,11 +76,10 @@ namespace SwaySettings {
                 }
             });
 
-            if (current_user == null || !current_user.is_loaded) {
-                current_user = user_manager.get_user (username);
-                current_user.notify["is-loaded"].connect ((er,t) => {});
-                current_user.changed.connect (set_user_data);
-            } else {
+            is_loaded_id = current_user.notify["is-loaded"].connect (set_user_data);
+            changed_id = current_user.changed.connect (set_user_data);
+
+            if (current_user.is_loaded) {
                 set_user_data ();
             }
 
@@ -169,25 +183,25 @@ namespace SwaySettings {
                 string path = Path.build_filename (file.get_path (),
                                                    info.get_name ());
                 switch (info.get_file_type ()) {
-                    case FileType.DIRECTORY :
-                        // Limit the search depth
-                        if (depth < max_depth) {
-                            depth++;
-                            get_avatars_in_path (path, formats,
-                                                 depth, max_depth);
-                        }
-                        break;
-                    case FileType.REGULAR:
-                        string suffix = path.slice (
-                            path.last_index_of_char ('.') + 1,
-                            path.length);
-                        if (suffix in formats) {
-                            content.popover_flowbox.add (
-                                new Popover_Image (path, set_user_img));
-                        }
-                        break;
-                    default:
-                        return;
+                        case FileType.DIRECTORY :
+                            // Limit the search depth
+                            if (depth < max_depth) {
+                                depth++;
+                                get_avatars_in_path (path, formats,
+                                                     depth, max_depth);
+                            }
+                            break;
+                        case FileType.REGULAR:
+                            string suffix = path.slice (
+                                path.last_index_of_char ('.') + 1,
+                                path.length);
+                            if (suffix in formats) {
+                                content.popover_flowbox.add (
+                                    new Popover_Image (path, set_user_img));
+                            }
+                            break;
+                        default:
+                            return;
                 }
             });
         }